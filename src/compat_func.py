c0d23cce102c2830974952711bcc2be9d90d8cfa
Rico Angell <rangell@cs.umass.edu>
2021-05-04 15:57:30 -0400

    fixed both compat_funcs

--- src/compat_func.py
+++ src/compat_func.py
@@ -4,7 +4,7 @@ import numpy as np
 def raw_overlap(node, constraint):
     rep = node.raw_rep
     pair_product = rep * constraint
-    if np.all((pair_product) >= 0):
+    if np.all(pair_product >= 0):
         return np.sum(pair_product > 0) / np.sum(constraint > 0)
     else:
         return -np.inf
@@ -12,8 +12,9 @@ def raw_overlap(node, constraint):
 
 def transformed_overlap(node, constraint):
     rep = node.transformed_rep
+    pair_product = rep * constraint
     if np.array_equal(rep, np.ones_like(rep) * -np.inf) \
-            or not np.all((rep * constraint) >= 0):
+            or not np.all(pair_product >= 0):
         return -np.inf
     else:
-        return np.sum(constraint == rep) / np.sum(constraint != 0)
+        return np.sum(pair_product > 0) / np.sum(constraint != 0)
